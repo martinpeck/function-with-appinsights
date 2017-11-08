a6099a0d643cd4014ae0862e50ebdc21008b8727
Martin Peck <martinpeck@Martins-MBP.guest.corp.microsoft.com>
2017-11-08 11:43:41 +0000

    fixed copypasta fail in FailFunction

--- FailFunction/index.js
+++ FailFunction/index.js
@@ -1,16 +1,12 @@
-module.exports = function (context, req) {
+var sharedValue = require('../shared/sharedstuff').shared;
 
-    var sharedValue = require('../shared/sharedstuff').shared;
-    
-    module.exports = function (context, req) {
-        context.log('JavaScript HTTP trigger function processed a request.');
-    
-        context.res = {
-            status: 400,
-            body: "Hello from FailFunction. Shared value:  " + sharedValue
-        };
-    
-        context.done();
+module.exports = function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    context.res = {
+        status: 400,
+        body: "Hello from FailFunction. Shared value:  " + sharedValue
     };
 
+    context.done();
 };
